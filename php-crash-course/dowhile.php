e5c1033c766aa553e075a05e3fd05f21f779cd11
Hazerin <alby91v@gmail.com>
2025-08-21 09:59:45 +0200

    checkpoint

---
+++ php-crash-course/dowhile.php
@@ -0,0 +1,14 @@
+<?php
+
+do {
+    $diceRoll = rand(1,6);
+    echo "You rolled a $diceRoll\n";
+
+    if (6 == $diceRoll) {
+        echo "Congrats! You hit the jackpot!";
+    }
+    echo "Roll again? (y/n)\n";
+
+    $rollAgain = trim(fgets(STDIN));
+
+} while ("y" == $rollAgain);
