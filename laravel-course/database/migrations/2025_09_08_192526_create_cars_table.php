598c6403a5735da098452cdb403ebff7dacf897c
Hazerin <alby91v@gmail.com>
2025-09-08 22:49:43 +0200

    checkpoint

---
+++ laravel-course/database/migrations/2025_09_08_192526_create_cars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("maker_id")->constrained("makers");
+            $table->foreignId("model_id")->constrained("models");
+            $table->integer("year");
+            $table->integer("price");
+            $table->integer("mileage");
+            $table->string("vin", 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
